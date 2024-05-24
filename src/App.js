dc3f3d82fdb5858f397fd233f5ff23d83629d2f6
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-05-24 18:40:19 +0500

    initialized basic CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, sint!
+        Quas, incidunt! Ipsum eligendi quaerat minus porro non eaque provident,
+        cumque nihil nemo mollitia, ratione eum maiores, blanditiis iusto sit?
+      </p>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <h2>Lorem, ipsum.</h2>
+      <p className="overline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cumque
+        perspiciatis cum? Labore impedit rem minus natus, id vel consectetur
+        inventore nihil est dolores ducimus voluptates ipsam in aperiam
+        praesentium?
+      </p>
     </div>
   );
-}
+};
 
 export default App;
