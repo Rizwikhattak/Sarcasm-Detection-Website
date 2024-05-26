a5923855a774a4c9b17271a9a4523dd2aacf7729
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-05-26 17:22:26 +0500

    starting with the navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./Components/Home/Home";
+import Navbar from "./Navbar/Navbar";
 const App = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, sint!
-        Quas, incidunt! Ipsum eligendi quaerat minus porro non eaque provident,
-        cumque nihil nemo mollitia, ratione eum maiores, blanditiis iusto sit?
-      </p>
-      <h1>Lorem ipsum dolor sit amet.</h1>
-      <h2>Lorem, ipsum.</h2>
-      <p className="overline">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cumque
-        perspiciatis cum? Labore impedit rem minus natus, id vel consectetur
-        inventore nihil est dolores ducimus voluptates ipsam in aperiam
-        praesentium?
-      </p>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
   );
 };
 
