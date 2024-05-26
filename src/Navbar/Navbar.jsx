b816029dbfd62b0c45decec8a360a11f885baef4
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-05-26 18:20:42 +0500

    Navbar Done

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosClose } from "react-icons/io";
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <>
       <header className="bg-white">
@@ -8,7 +10,7 @@ const Navbar = () => {
           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
           aria-label="Global"
         >
-          <div className="flex lg:flex-1">
+          <div className="lg:flex lg:flex-1">
             <button className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <h1 className="logo">SARCASM</h1>
@@ -17,23 +19,13 @@ const Navbar = () => {
           <div className="flex lg:hidden">
             <button
               type="button"
+              onClick={() => {
+                console.log("Done");
+                setToggleMenu(!toggleMenu);
+              }}
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
+              <RxHamburgerMenu />
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
@@ -58,72 +50,59 @@ const Navbar = () => {
           </div>
         </nav>
         {/* Mobile menu, show/hide based on menu open state. */}
-        <div className="lg:hidden" role="dialog" aria-modal="true">
-          {/* Background backdrop, show/hide based on slide-over state. */}
-          <div className="fixed inset-0 z-10" />
-          <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <button className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </button>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Close menu</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
+
+        {toggleMenu && (
+          <div className="lg:hidden" role="dialog" aria-modal="true">
+            {/* Background backdrop, show/hide based on slide-over state. */}
+            <div className="fixed inset-0 z-10" />
+            <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex md:hidden items-center justify-between">
+                <button className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                  <h1 className="logo">SARCASM</h1>
+                </button>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={() => setToggleMenu(!toggleMenu)}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  <div className="-mx-3">
-                    <button
-                      type="button"
-                      className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                      aria-controls="disclosure-1"
-                      aria-expanded="false"
-                    >
-                      Product
+                  <IoIosClose />
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    <div className="-mx-3">
+                      <button
+                        type="button"
+                        className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        aria-controls="disclosure-1"
+                        aria-expanded="false"
+                      >
+                        Product
+                      </button>
+                    </div>
+                    <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Features
+                    </button>
+                    <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Marketplace
+                    </button>
+                    <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Company
                     </button>
                   </div>
-                  <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                    Features
-                  </button>
-                  <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                    Marketplace
-                  </button>
-                  <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                    Company
-                  </button>
-                </div>
-                <div className="py-6">
-                  <button className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                    Log in
-                  </button>
+                  <div className="py-6">
+                    <button className="BodyLarge text-gray-900">Log in</button>
+                    <button className="BodyLarge prim-color text-gray-900">
+                      Sign Up
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </header>
     </>
   );
